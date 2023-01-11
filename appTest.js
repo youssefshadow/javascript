653f0973b3451edf5ae1925a925a4b91cc15f1ed
youssefshadow <youssef20ben@gmail.com>
2023-01-11 10:23:57 +0100

    test++

--- appTest.js
+++ appTest.js
@@ -1,10 +1,19 @@
 let text=document.querySelector('.truc');
+let container=document.querySelector('.container')
 document.getElementById("myBtn").addEventListener("click", myFunction);
 
 function myFunction() {
-  text.innerHTML=Math.floor(Math.random() * 10100000);
+  text.innerText=Math.floor(Math.random() * 10100000);
+  if ( text.innerText%5==0) {
+    text.style.color = "green";
+    container.style.boxShadow =" 0px 5px 15px green";
+  }else{
+    text.style.color = "red";
+    container.style.boxShadow =" 0px 5px 15px red";
+  }
 
 
 }
 
 console.log('floflo');
+
