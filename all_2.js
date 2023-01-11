c6e78e91af110e05d83a8a5d807143ddf12c599c
youssefshadow <youssef20ben@gmail.com>
2023-01-11 14:19:31 +0100

    florent le magnifique

---
+++ all_2.js
@@ -0,0 +1,13 @@
+// Exercice all2_1 :	écrire une fonction qui retourne **la position** du plus grand élément d'un tableau.
+// Exemple : position_biggest([4,7,5,5,6]) retourne 1 car le plus grand est dans la case numéro 1
+tabX=[1,2,11,3,4,5,6,7];
+function position(tableau) {
+    let somme = Math.max(...tableau);
+    return tableau.indexOf(somme);
+}
+console.log(position(tabX));
+// Exercice all2_2 : écrire une fonction qui fait la somme de tous les éléments d'un tableau.
+
+// Exercice all2_3 : écrire une fonction qui retourne la somme des valeurs positives d'une liste.
+
+// Exercice all2_4 : En utilisant les deux fonctions précédentes, écrire une fonction qui retourne la somme des valeurs négatives d'une liste.
